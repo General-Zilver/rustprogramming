28e268c99fe6ea2fd2708ded42329b50ddf001e4
General-Zilver <juan.payes01@utrgv.edu>
2025-09-10 14:33:31 +0000

    First Commit

---
+++ my_project/src/main.rs
@@ -0,0 +1,4 @@
+fn main() 
+{
+    println!("Hello, John A Payes Jr!");
+}
---
+++ my_project/target/debug/deps/my_project-59e9ebcae080e135.d
@@ -0,0 +1,5 @@
+/workspaces/rustprogramming/my_project/target/debug/deps/my_project-59e9ebcae080e135.d: src/main.rs
+
+/workspaces/rustprogramming/my_project/target/debug/deps/libmy_project-59e9ebcae080e135.rmeta: src/main.rs
+
+src/main.rs:
